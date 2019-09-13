0155849eeeb6ff84cc7ace50d4d26714437c0e38
Kevin <kevin.aberg.kultalahti@rhyme.se>
2019-09-13 15:27:01 +0200

    chore: bump version numbers, add svelte-image. WIP: get svelte-image to work correctly and take arguments.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,18 @@
-<style>
+<script>
+  import Image from "svelte-image";
+</script>
 
+<style>
+  div {
+    max-width: 500px;
+  }
 </style>
 
 <svelte:head>
   <title>Kevin Åberg Kultalahti's Personal Website</title>
 </svelte:head>
+
+<div>
+  <Image alt="test" src="test.jpg" />
+  <Image alt="test" src="great-success.png" />
+</div>
