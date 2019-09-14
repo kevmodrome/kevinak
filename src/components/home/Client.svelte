6ca73975bb1de06121d3f4ffa1014de2252ba648
Kevin <kevin.aberg.kultalahti@rhyme.se>
2019-09-14 22:18:18 +0200

    wip: adds client component with basic styling. TODO: explore mobile views variations.

---
+++ src/components/home/Client.svelte
@@ -0,0 +1,66 @@
+<style>
+  .container {
+    display: flex;
+    flex-direction: column;
+  }
+  .text {
+    flex: 1;
+    margin-right: 0;
+    margin-bottom: 10px;
+  }
+  .title {
+    margin-bottom: 10px;
+  }
+  .image {
+    flex: 1;
+  }
+  img {
+    width: 100%;
+    max-height: 200px;
+    object-fit: cover;
+  }
+  @media (min-width: 500px) {
+    .container {
+      flex-direction: row;
+    }
+    .text {
+      flex: 3;
+      margin-right: 20px;
+      margin-bottom: 0;
+    }
+    .image {
+      flex: 1;
+    }
+  }
+  @media (min-width: 1080px) {
+    .container {
+      flex-direction: row;
+    }
+    .text {
+      flex: 3;
+      margin-right: 20px;
+      margin-bottom: 0;
+    }
+    .image {
+      flex: 1;
+    }
+  }
+</style>
+
+<div class="container">
+  <div class="text">
+    <div class="title">Mäklarsamfundets Kundvägledarna</div>
+    <div class="text">
+      Mäklarsamfundet wanted a website for their new service
+      <span>
+        <a href=".">Kundvägledarna</a>
+      </span>
+      . The idea in short is about helping real estate buyers and sellers by
+      offering a plethora of frequently asked questions as well a chat
+      functionality where related questions can be asked.
+    </div>
+  </div>
+  <div class="image">
+    <img src="test.jpg" alt="Mäklarsamfundet Placeholder" />
+  </div>
+</div>
