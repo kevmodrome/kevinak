52475a46ea4c71bb9aa1e8cc90ae5f39eec0fa05
Kevin <kevin.aberg.kultalahti@rhyme.se>
2019-09-15 12:38:32 +0200

    fix: adjust css for button component.

--- src/components/common/Button.svelte
+++ src/components/common/Button.svelte
@@ -6,25 +6,52 @@
 
 <style>
   button {
+    position: relative;
     padding: 10px 15px;
     color: white;
+    font-weight: bold;
+    font-size: 14px;
     background: #353f99;
-    font-size: 24px;
     border-radius: 8px;
+    border: none;
+    transition: outline 0.6s linear;
   }
   .outline {
     background: transparent;
-    border: 2px solid white;
+  }
+  .outline:after {
+    content: "";
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    border: solid 3px white;
+    border-radius: 8px;
+    z-index: -1;
+  }
+  .outline:hover::after {
   }
   .primaryOutline {
     background: transparent;
     color: #353f99;
-    border: 1px solid #353f99;
+    border: solid 3px #353f99;
+    border-radius: 8px;
+  }
+  .primaryOutline {
+    color: black;
+    border: solid 3px black;
   }
   .blackOutline {
     background: transparent;
     color: black;
-    border: 1px solid black;
+    border: solid 3px black;
+    border-radius: 8px;
+  }
+  .blackOutline:hover {
+    color: #353f99;
+    border: solid 3px #353f99;
   }
 </style>
 
