193583f49880e4cd87ac83f9d9458c9f6d51fbea
Kevin <kevin.aberg.kultalahti@rhyme.se>
2019-09-19 17:01:18 +0200

    feat: adds a static instagram component with all styling done.

--- src/components/home/Instagram.svelte
+++ src/components/home/Instagram.svelte
@@ -1,22 +1,88 @@
 <style>
   .container {
     margin: 20px auto;
+    margin-bottom: 40px;
     display: flex;
+    flex-direction: column;
     justify-content: center;
   }
 
-  @media (min-width: 500px) {
+  .grid {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-auto-rows: 1fr;
+    grid-column-gap: 20px;
+    grid-row-gap: 20px;
+  }
+  .grid::before {
+    content: "";
+    width: 0;
+    padding-bottom: 100%;
+    grid-row: 1 / 1;
+    grid-column: 1 / 1;
+  }
+
+  .grid > *:first-child {
+    grid-row: 1 / 1;
+    grid-column: 1 / 1;
+  }
+
+  .item {
+    background: green;
+    position: relative;
+  }
+
+  img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    padding: 0;
+  }
+
+  h2 {
+    margin-bottom: 30px;
+    margin-left: 20px;
+  }
+
+  @media (min-width: 600px) {
     .container {
       margin: 50px auto;
+      margin-bottom: 60px;
+    }
+    .grid {
+      grid-template-columns: 1fr 1fr 1fr 1fr;
     }
   }
   @media (min-width: 1080) {
     .container {
       margin: 80px auto;
+      margin-bottom: 60px;
+    }
+    .grid {
+      grid-template-columns: 1fr 1fr 1fr 1fr;
     }
   }
 </style>
 
 <div class="container">
-  <div>Instagram goes here</div>
+  <h2>My Tech Travels</h2>
+  <div class="grid">
+    <div class="item">
+      <img src="test.jpg" alt="Cool instagram pic." />
+    </div>
+    <div class="item">
+      <img src="test.jpg" alt="Cool instagram pic." />
+    </div>
+    <div class="item">
+      <img src="test.jpg" alt="Cool instagram pic." />
+    </div>
+    <div class="item">
+      <img src="test.jpg" alt="Cool instagram pic." />
+    </div>
+  </div>
 </div>
