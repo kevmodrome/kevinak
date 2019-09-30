37cd3463901bf75066de0b8ea6e34b5c9dace03a
Kevin <kevin.aberg.kultalahti@rhyme.se>
2019-09-30 18:40:24 +0200

    wip: adds copy of index page as starting skeleton to about page.

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,3 +1,33 @@
+<script>
+  import Divider from "../components/common/Divider.svelte";
+
+  import Headline from "../components/home/Headline.svelte";
+  import About from "../components/home/About.svelte";
+  import Work from "../components/home/Work.svelte";
+  import Skills from "../components/home/Skills.svelte";
+  import Instagram from "../components/home/Instagram.svelte";
+  import Footer from "../components/Footer.svelte";
+</script>
+
+<style>
+  div {
+    margin-bottom: 100px;
+    margin: 0 auto;
+  }
+</style>
+
 <svelte:head>
-  <title>About</title>
+  <title>Kevin Åberg Kultalahti's Personal Website - About</title>
 </svelte:head>
+
+<div>
+  <Headline />
+  <Divider />
+  <About />
+  <Divider />
+  <Work />
+  <Divider />
+  <Skills />
+  <Divider />
+  <Instagram />
+</div>
